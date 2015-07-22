5a15e40d94a100b925954a2d6939756494a4d5a0
guillaumewf3 <guillaumewf3@gmail.com>
2015-07-22 10:37:00 +0200

    Switch some content to french, add forgot passwork link

--- login.php
+++ login.php
@@ -8,12 +8,17 @@
 	<title>Connexion !</title>
 </head>
 <body>
+
+	<h1>Connexion</h1>
 	
 	<form method="POST" action="login_handler.php">
-		<input type="text" name="email" placeholder="Email or username" />
-		<input type="password" name="password" placeholder="Password" />
+		<input type="text" name="email" placeholder="Email ou pseudo" />
+		<input type="password" name="password" placeholder="Mot de passe" />
 		<input type="submit" value="OK" />
 	</form>
+
+	<a href="forgot_password.php">Mot de passe oublié ?</a>
+
 	<?php 
 		//si on a stocké un message d'erreur (dans login_handler.php)
 		if(!empty($_SESSION['login_error'])){
